9588b86dd4a083a6435d4f68883fa0bc4c6b325d
Virág Ádám <viragadam100@gmail.com>
2022-11-09 16:50:49 +0100

    done

--- fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { ExchangeRate } from '@cuid/entities';
+import {Injectable} from '@nestjs/common';
+import {ExchangeRate} from '@cuid/entities';
+
+const axios = require('axios');
+const xml2js = require('xml2js');
+
 
 @Injectable()
 export class ExchangeRateService {
+    constructor() {
+    }
+
     public getExchangeRates = async () => {
         const rates: ExchangeRate[] = [];
+        const response = await axios.get("https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml");
+        const parsedData = await xml2js.parseStringPromise(response.data);
+        const exchangeData = parsedData.kurzy.tabulka[0].radek;
+        exchangeData.forEach((item: any) => {
+            const itemData = item['$'];
+            const rate = new ExchangeRate();
+            rate.country = itemData.zeme;
+            rate.currency = itemData.mena;
+            rate.amount = itemData.mnozstvi;
+            rate.code = itemData.kod;
+            rate.rate = itemData.kurz.replace(',', '.');
+            rates.push(rate);
+        });
 
-        // TODO: Implement the fetching and parsing of the exchange rates.
-        // Use this method in the resolver.
 
         return rates;
     };
--- fullstack/task/packages/web/src/App.tsx
+++ fullstack/task/packages/web/src/App.tsx
@@ -1,10 +1,65 @@
-function App() {
-    // TODO: Add the FE for exchange rates here.
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, withStyles} from "@mui/material";
+import {gql, useQuery} from "@apollo/client";
+import {ExchangeRate} from "@cuid/server/dist/entities";
 
+
+const getExchangeRates = gql(`
+    query{
+        exchangeRates{
+            currency
+            rate
+            amount
+            code
+            country
+    }
+    }`);
+
+
+function App() {
+    const {data, loading, error} = useQuery(getExchangeRates);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+
+    const rows: ExchangeRate[] = data?.exchangeRates;
     return (
-        <div>
-            <p>Add the exchange rates here</p>
-        </div>
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+                <TableHead  sx={{
+                    "& th": {
+                        fontWeight: "bold",
+                        backgroundColor: "#3f51b5",
+                    }
+                }}
+                >
+                    <TableRow>
+                        <TableCell>Country</TableCell>
+                        <TableCell align="right">Currency</TableCell>
+                        <TableCell align="right">Amount</TableCell>
+                        <TableCell align="right">Code</TableCell>
+                        <TableCell align="right">Rate</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows?.map((row) => (
+                        <TableRow
+                            key={row.country}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.country}
+                            </TableCell>
+                            <TableCell align="right">{row.currency}</TableCell>
+                            <TableCell align="right">{row.amount}</TableCell>
+                            <TableCell align="right">{row.code}</TableCell>
+                            <TableCell align="right">{row.rate}</TableCell>
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+        </TableContainer>
+
     );
 }
 
