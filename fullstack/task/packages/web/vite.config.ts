bc263534f86c4e09162bbfcb9bd4bd9cd9faeadd
b4rt <root.b4rt@gmail.com>
2023-04-09 09:58:46 -0500

    feat[web]: Changing port

--- fullstack/task/packages/web/vite.config.ts
+++ fullstack/task/packages/web/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3000
+        port: 3001
     },
     preview: {
         port: 8080
