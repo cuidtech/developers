cc7226c602b0b787f22cf41028e9f47d99316fd1
Nacho Sabatini <nacho.sabatini9@gmail.com>
2023-05-15 15:20:11 -0300

    Creating Graphql Query in BE

--- fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.resolver.ts
+++ fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.resolver.ts
@@ -2,13 +2,13 @@ import { Query, Resolver } from '@nestjs/graphql';
 import { ExchangeRate } from '@cuid/entities';
 import { ExchangeRateService } from './exchange-rate.service';
 
-@Resolver(ExchangeRate)
+@Resolver(() => ExchangeRate)
 export class ExchangeRateResolver {
     constructor(private readonly exchangeRateService: ExchangeRateService) {}
 
     // TODO: Implement a GraphQL Query that returns the exchange rates
-    @Query(() => String)
-    async exchangeRates(): Promise<String> {
-        return 'Hello';
+    @Query(() => [ExchangeRate])
+    async exchangeRates(): Promise<ExchangeRate[]> {
+        return this.exchangeRateService.getExchangeRates();
     }
 }
--- fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
@@ -1,14 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { ExchangeRate } from '@cuid/entities';
+import { parseStringPromise } from 'xml2js';
+import fetch from 'node-fetch';
 
+// This is the official API from CNB (Czech National Bank), the only problem is that is in XML format and in czech language
+// There was anothers alternatives but they are not official and they are not updated so I preffered to use this one
+// I create a parser to convert the XML to JSON and then I can use it
+const apiURL =
+    'https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml';
+
+// Parser to convert the XML to JSON and then I can use it
+async function parseExchangeRatesFromXml(xmlData: string): Promise<ExchangeRate[]> {
+    const jsonData = await parseStringPromise(xmlData, {
+        explicitArray: false, // Ensure objects are not wrapped in arrays
+    });
+
+    const rows = jsonData?.kurzy?.tabulka?.radek;
+
+    if (!Array.isArray(rows)) {
+        return [];
+    }
+
+    const exchangeRates: ExchangeRate[] = rows.map((row) => {
+        const country = row.$.zeme;
+        const currency = row.$.mena;
+        const code = row.$.kod;
+        const rate = row.$.kurz?.replace(',', '.');
+        const amount = Number(row.$.mnozstvi);
+
+        return { currency, code, amount, country, rate };
+    });
+
+    return exchangeRates;
+}
 @Injectable()
 export class ExchangeRateService {
-    public getExchangeRates = async () => {
-        const rates: ExchangeRate[] = [];
-
+    async getExchangeRates(): Promise<ExchangeRate[]> {
         // TODO: Implement the fetching and parsing of the exchange rates.
         // Use this method in the resolver.
+        const response = await fetch(apiURL);
+        const xmlData = await response.text();
+        const exchangeRates = await parseExchangeRatesFromXml(xmlData);
 
-        return rates;
-    };
+        return exchangeRates;
+    }
 }
