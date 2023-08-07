4447867fe947e4b278ce86becef3a0b22b57e3c9
Peter Grilli (tu1ly) <grilli.peter@operatorict.cz>
2023-08-08 00:48:49 +0200

    fix: default cnb url

--- fullstack/task/packages/server/src/config/cnb/index.ts
+++ fullstack/task/packages/server/src/config/cnb/index.ts
@@ -3,7 +3,5 @@ import { config } from 'dotenv';
 config();
 
 export const cnbConfig = {
-    url:
-        process.env.CNB_RATES_URL ||
-        'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt',
+    url: process.env.CNB_RATES_URL || 'https://api.cnb.cz/cnbapi/exrates/daily',
 };
