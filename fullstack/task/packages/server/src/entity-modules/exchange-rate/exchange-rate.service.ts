c5e4a73efe12c6998c1f0f4bc648b15cf9e3d034
tomasn <tomasnovella@gmail.com>
2023-08-11 15:02:50 +0200

    Add HttpService

--- fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
+++ fullstack/task/packages/server/src/entity-modules/exchange-rate/exchange-rate.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ExchangeRate } from '@cuid/entities';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class ExchangeRateService {
+    constructor(private configService: ConfigService, private readonly httpService: HttpService) {}
+
     public getExchangeRates = async () => {
-        console.log('!!! service');
+        const cnbEndpoint = this.configService.get('CNB_URL');
+        console.log('!!! service', cnbEndpoint);
         const rates: ExchangeRate[] = [
             { country: 'test-country', currency: 'czk', amount: 4444, code: 'CODE', rate: 111.2 },
         ];
@@ -12,6 +17,6 @@ export class ExchangeRateService {
         // TODO: Implement the fetching and parsing of the exchange rates.
         // Use this method in the resolver.
 
-        return rates;
+        return Promise.resolve(rates);
     };
 }
