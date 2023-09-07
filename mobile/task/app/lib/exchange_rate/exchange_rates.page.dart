1c072559d2192ef1c1fb1d26c282c905236ee608
Petr Povala <petr@povala.cz>
2023-09-07 10:31:26 +0200

    #load initial data

--- mobile/task/app/lib/exchange_rate/exchange_rates.page.dart
+++ mobile/task/app/lib/exchange_rate/exchange_rates.page.dart
@@ -1,11 +1,31 @@
 import 'package:app/page.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class ExchangeRatesPage extends StatelessWidget {
   const ExchangeRatesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PageWidget(title: 'Exchange Rates', child: Container());
+    String readExchangeRates = """
+      query { exchangeRates {code description rates}}
+    """;
+    return PageWidget(
+        title: 'Exchange Rates',
+        child: Query(
+          options: QueryOptions(document: gql(readExchangeRates)),
+          builder: (QueryResult result,
+              {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+
+            if (result.isLoading) {
+              return const Text('Loading');
+            }
+
+            return Container();
+          },
+        ));
   }
 }
