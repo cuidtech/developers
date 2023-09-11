ea1f9d69b8d0e44054f863b033e98a249197aef0
Tomas Baran <tomas.baran@gmail.com>
2023-09-11 09:36:22 -0600

    refactor: separate graphQL service class

--- mobile/task/lib/main.dart
+++ mobile/task/lib/main.dart
@@ -1,26 +1,9 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:task/services/graphql_service.dart';
 
 void main() async {
-  final HttpLink httpLink = HttpLink(
-    'http://localhost:4000/graphql',
-  );
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      link: httpLink,
-      cache: GraphQLCache(),
-    ),
-  );
-
-  runApp(
-    GraphQLProvider(
-      client: client,
-      child: const MainApp(),
-    ),
-  );
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -28,32 +11,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Query(
-              options: QueryOptions(
-                document: gql(
-                  '''
-                  query {
-  exchangeRate(code: "EUR") {
-    code
-  }
-}
-                ''',
+    return GraphQLProvider(
+      client: GraphQLService.client,
+      child: MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Query(
+                options: QueryOptions(
+                  document: gql(
+                    '''
+                    query {
+      exchangeRate(code: "EUR") {
+      code
+      }
+    }
+                  ''',
+                  ),
                 ),
-              ),
-              builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
-                if (result.hasException) {
-                  return Text(result.exception.toString());
-                }
+                builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
+                  if (result.hasException) {
+                    return Text(result.exception.toString());
+                  }
 
-                if (result.isLoading) {
-                  return const CircularProgressIndicator();
-                }
-                debugPrint(result.data.toString());
-                return Text(result.data.toString());
-              }),
+                  if (result.isLoading) {
+                    return const CircularProgressIndicator();
+                  }
+                  debugPrint(result.data.toString());
+                  return Text(result.data.toString());
+                }),
+          ),
         ),
       ),
     );
