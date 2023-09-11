82e55ef37a417ebba34536ba1724f484b559f23b
Tomas Baran <tomas.baran@gmail.com>
2023-09-11 09:47:04 -0600

    feat: show currencies list

--- mobile/task/lib/screens/list_screen.dart
+++ mobile/task/lib/screens/list_screen.dart
@@ -7,31 +7,45 @@ class ListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Query(
-            options: QueryOptions(
-              document: gql(
-                '''
-                    query {
-      exchangeRate(code: "EUR") {
-      code
-      }
-    }
-                  ''',
-              ),
-            ),
-            builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
-              if (result.hasException) {
-                return Text(result.exception.toString());
-              }
-
-              if (result.isLoading) {
-                return const CircularProgressIndicator();
-              }
-              debugPrint(result.data.toString());
-              return Text(result.data.toString());
-            }),
+      appBar: AppBar(
+        title: const Text('Exchange Rates'),
       ),
+      body: Query(
+          options: QueryOptions(
+            document: gql(
+              '''
+query {
+  exchangeRates(historyLength: 2) {
+    code
+    rates
+  }
+}
+
+                ''',
+            ),
+          ),
+          builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+
+            if (result.isLoading) {
+              return const CircularProgressIndicator();
+            }
+
+            // debugPrint(result.data.toString());
+            return ListView(
+              children: [
+                for (final currency in result.data!['exchangeRates'])
+                  ListTile(
+                      title: Text(currency['code']),
+                      subtitle: Text(currency['rates'].first.toStringAsFixed(2)),
+                      trailing: currency['rates'].first > currency['rates'].last
+                          ? const Icon(Icons.arrow_upward, color: Colors.green)
+                          : const Icon(Icons.arrow_downward, color: Colors.red)),
+              ],
+            );
+          }),
     );
   }
 }
