50e286cd442a6e2d734ae9f6f9d879c27dc92f80
Tomas Baran <tomas.baran@gmail.com>
2023-09-11 09:28:25 -0600

    config: graphQL setup

--- mobile/task/lib/main.dart
+++ mobile/task/lib/main.dart
@@ -1,7 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() async {
+  final HttpLink httpLink = HttpLink(
+    'http://localhost:4000/graphql',
+  );
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(),
+    ),
+  );
+
+  runApp(
+    GraphQLProvider(
+      client: client,
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +28,32 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Query(
+              options: QueryOptions(
+                document: gql(
+                  '''
+                  query {
+  exchangeRate(code: "EUR") {
+    code
+  }
+}
+                ''',
+                ),
+              ),
+              builder: (QueryResult result, {VoidCallback? refetch, FetchMore? fetchMore}) {
+                if (result.hasException) {
+                  return Text(result.exception.toString());
+                }
+
+                if (result.isLoading) {
+                  return const CircularProgressIndicator();
+                }
+                debugPrint(result.data.toString());
+                return Text(result.data.toString());
+              }),
         ),
       ),
     );
